ebc90dd7a68f71360436ea0f3559123502373418
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-04-20 12:05:33 -0500

    modelos y migraciones

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    id_employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    id_project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    date_start = models.DateField()
+    estimate_time = models.IntegerField()
+    status = models.CharField(max_length=50)
+
+    def estimated_end(self):
+        return self.date_start + timedelta(days=self.estimate_time)
+
+    def __str__(self):
+        return self.description
