19f87b874adbc18b7e777d6849c113ef1e1de1c1
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-04-20 16:04:26 -0500

    RENDER

--- gestor_de_tareas/build.sh
+++ gestor_de_tareas/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
+
+# Salir si ocurre un error
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
+# Ejecutar migraciones de la base de datos
 python manage.py migrate
+
+# Colectar archivos estáticos (por si usás css/js en templates)
+python manage.py collectstatic --noinput
